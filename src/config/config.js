89f2b219a929eba9e1208a967c746766543a2642
goldmembrane <byenghyunhwang@Byengui-MacBook-Pro.local>
2020-07-29 13:24:49 +0900

    Pending connect RDS to EC2

--- src/config/config.js
+++ src/config/config.js
@@ -2,7 +2,7 @@ module.exports = {
   development: {
     username: "root",
     password: process.env.DATABASE_PASSWORD,
-    database: "AAASUL",
+    database: "SUL_database",
     host: "sul-database-deploy.csmeh7i5hzpa.ap-northeast-2.rds.amazonaws.com",
     port: 3306,
     dialect: "mysql",
