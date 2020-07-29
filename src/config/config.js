52284a9750ffb3abbc607e5eed6e21484ed0907b
goldmembrane <49430407+goldmembrane@users.noreply.github.com>
2020-07-29 14:59:07 +0900

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -3,7 +3,7 @@ module.exports = {
     username: "root",
     password: process.env.DATABASE_PASSWORD,
     database: "SUL",
-    host: "localhost",
+    host: "aaasul.csmeh7i5hzpa.ap-northeast-2.rds.amazonaws.com",
     port: 3306,
     dialect: "mysql",
     logging: false,
