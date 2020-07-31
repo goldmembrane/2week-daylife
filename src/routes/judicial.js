a54634d1628adcc56851718c0c21deed3d963a1c
goldmembrane <byenghyunhwang@Byengui-MacBook-Pro.local>
2020-07-31 11:05:24 +0900

    Completed Refactoring "compress judicial method into post"

--- src/routes/judicial.js
+++ src/routes/judicial.js
@@ -4,6 +4,5 @@ const router = express.Router();
 const { judicialController } = require("../controllers");
 
 router.post("/post", judicialController.judicial.post);
-router.get("/get", judicialController.judicial.get);
 
 module.exports = router;
