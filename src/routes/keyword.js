202891a703ebd122ed9201212645c303fc2730f3
goldmembrane <byenghyunhwang@Byengui-MacBook-Pro.local>
2020-08-04 10:21:49 +0900

    Completed fix keyword routing to search/post

--- src/routes/keyword.js
+++ src/routes/keyword.js
@@ -4,7 +4,6 @@ const router = express.Router();
 const { keywordController } = require("../controllers");
 
 router.get("/get", keywordController.keyword.get);
-router.post("/post", keywordController.keyword.post);
 router.delete("/delete", keywordController.keyword.delete);
 
 module.exports = router;
