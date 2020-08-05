4f2f1e37dbead4239ab52c27e81b442aeb38da23
김준섭 <kimjunseob@gimjunseob-ui-MacBookPro.local>
2020-08-05 21:57:02 +0900

    Modify cors origin

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(morgan("dev"));
 
 app.use(
   cors({
-    origin: "http://localhost:3001",
+    origin: "http://localhost:3000",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
